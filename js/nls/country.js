f46fc52fae1393137e66c780ef25ba7af54177b8
unknown <Matthieu@.(none)>
2013-07-30 22:21:06 +0200

    Create a ressource for the country name

---
+++ js/nls/country.js
@@ -0,0 +1,24 @@
+define({
+    /* Please sort alphabetically */
+    "root" : {
+       AR : "Argentine",
+       BR : "Brésil",
+       DE : "Allemagne",
+       EN : "Angleterre",
+       ES : "Espagne",
+       FR : "France",
+       GE : "Georgie",
+       IT : "Italie",
+       NL : "Pays-Bas",
+       PO : "Portugal",
+       RO : "Roumanie",
+       RT : "République Tchèque",
+       RU : "Russie",
+       SC : "Ecosse",
+       SU : "Suisse",
+       SW : "Suède",
+       TU : "Turquie",
+       UK : "Ukraine",
+       UR : "Uruguay"
+    }
+});
