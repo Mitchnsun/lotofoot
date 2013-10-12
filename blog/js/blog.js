2a2556b79d0418ab5920c0f001bb91653c76b645
Matthieu Compérat <matthieu.comperat@gmail.com>
2013-10-12 20:21:47 +0200

    Create the blog Put information for the version 0.1

---
+++ blog/js/blog.js
@@ -0,0 +1,25 @@
+var urlHTMLDefault = 'version/blogv0.1.html';
+
+/* DOM Ready and events function */
+$(document).ready(function(){
+	
+	// Initialize
+	$.ajax(urlHTMLDefault).done(function(data){
+		$('div.content').html(data);
+	});
+	
+	// Events
+	$('.version-button').click(function(e){
+		events.versionButton(e);
+	});
+});
+
+// Handle click callback
+var events = {
+	versionButton : function(e){
+		var url = 'version/blogv' + $(e.currentTarget).attr('ref') + '.html';
+		$.ajax(url).done(function(data){
+			$('div.content').html(data);
+		});
+	}
+};
