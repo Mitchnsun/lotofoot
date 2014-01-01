ff36b6656fb384f6982887b69b38fac2f3452277
Matthieu Compérat <matthieu.comperat@gmail.com>
2014-01-01 22:16:25 +0100

    [Blog] Add alert css from Bootstrap v0.1 fix image urls v0.2 add versionName poll with localStorage setting

--- blog/js/blog.js
+++ blog/js/blog.js
@@ -1,26 +1,84 @@
-var urlHTMLDefault = 'version/blogv0.2.html';
+var version = '0.2'; // Default version
+var urlHTMLDefault = 'version/blogv' + version + '.html';
 
 /* DOM Ready and events function */
-$(document).ready(function(){
-	
-	// Initialize
-	$.ajax(urlHTMLDefault).done(function(data){
-		$('div.content').html(data);
-	});
-	
-	// Events
-	$('.version-button').click(function(e){
-		events.versionButton(e);
-	});
+$(document).ready(function() {
+
+  // Initialize
+  webService.loadBlog(urlHTMLDefault);
+
+  //General events
+  $('.version-button').on("click", events.versionButton);
+
 });
 
-// Handle click callback
+// Events for each version page
+function bindVersionEvents() {
+  if (version == "0.1") {
+
+  } else if (version == "0.2") {
+    $('#formVersionName').on("submit", events.pollForNameVersion);
+  }
+}
+
+// Manage HTML DOM for each version page
+function customDisplayForVersion() {
+  if (version == "0.1") {
+
+  } else if (version == "0.2") {
+    if ( typeof localStorage == undefined) {
+      $("#formVersionName").remove();
+      $("#pollResults").show();
+      $("#pollResults .alert").html(text.alert_localStorage);
+      webService.loadPollResult('versionName');
+    }/*else if (localStorage["versionName" + version] !== undefined){
+      $("#formVersionName").remove();
+      $("#pollResults").show();
+      webService.loadPollResult('versionName');
+    }*/
+  }
+}
+
+// Handle events callback
 var events = {
-	versionButton : function(e){
-		e.preventDefault();
-		var url = 'version/blogv' + $(e.currentTarget).attr('ref') + '.html';
-		$.ajax(url).done(function(data){
-			$('div.content').html(data);
-		});
-	}
+  versionButton : function(e) {
+    e.preventDefault();
+    version = $(e.currentTarget).attr('ref');
+    var url = 'version/blogv' + version + '.html';
+    webService.loadBlog(url);
+  },
+  pollForNameVersion : function(e) {
+    e.preventDefault();
+    var choice = $("#formVersionName input:checked").val();
+    if (choice === undefined) {
+      $("#formVersionName .alert").show();
+    } else {
+      $("#formVersionName").remove();
+      $("#pollResults").show();
+      localStorage["versionName" + version] = choice;
+      webService.loadPollResult('versionName',choice);
+    }
+    // put the choice in local storage to choice once
+  }
+};
+
+// All ajax call
+var webService = {
+  loadBlog : function(url){
+    $.ajax(url).done(function(data) {
+      $('div.content').html(data);
+      bindVersionEvents();
+      customDisplayForVersion();
+    });
+  },
+  loadPollResult : function(type,choice){
+    console.log(type,version,choice);
+  }
+};
+
+/*
+ * 
+ */
+var text = {
+  alert_localStorage : "Vous ne pouvez accédez au sondage, votre navigateur ne supporte pas une fonctionnalité nécessaire pour le bon fonctionnement du vote. Veuillez mettre à jour votre navigateur."
 };
