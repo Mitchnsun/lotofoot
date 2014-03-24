0b1bf095fabbef26b9e248a1201b45f9e21f89b9
matthieu_comperat <matthieu.comperat@gmail.com>
2014-03-24 01:29:05 +0100

    [v0.2] Combine all urls, front and server side

--- js/fmk/urls.js
+++ js/fmk/urls.js
@@ -1,8 +1,23 @@
 define([], function() {
     return {
+    		/* Front */
         HOME : "",
         LOGIN : "login",
         PRONOS : "pronos",
-        CREATE_GAMES : "create-games"
+        CREATE_GAMES : "create-games",
+        
+        /* Server */
+       	/* Authentication */
+				URL_LOGIN : "server/authentication/login.php",
+				URL_CHECK_SESSION : "server/authentication/checkSession.php",
+				
+				/* Pronos */
+				URL_GET_NEW_PRONOS : "server/pronos/getNewPronos.php",
+				URL_ADD_PRONO : "server/pronos/addProno.php",
+				URL_CREATE_GAMES : "server/pronos/creategames.php",
+				URL_GET_PRONOS : "server/pronos/getpronos.php",
+				
+				/* Get Common datas */
+				URL_GET_TEAMS : "server/common/getTeams.php"
     };
 });
