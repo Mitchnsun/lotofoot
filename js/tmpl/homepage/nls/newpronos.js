6f3e9cdfa746dbd5d72394dc11a020b1b909a607
matthieu_comperat <matthieu.comperat@gmail.com>
2014-04-21 09:31:07 +0200

    [v0.2] Add an ad on homepage. Finish homepage

--- js/tmpl/homepage/nls/newpronos.js
+++ js/tmpl/homepage/nls/newpronos.js
@@ -3,6 +3,7 @@ define({
 	"root" : {
 		"last_pronos" : "Mes derniers pronostics",
 		"title" : "Pronostics du jour",
+		"no_pronos" : "Aucun pronostic n'est en cours.",
 		
 		/* Alert Success */
 		"AddProno" : "Votre pronostic a été enregistré",
