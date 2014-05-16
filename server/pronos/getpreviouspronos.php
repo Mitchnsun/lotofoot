90c5713596388131a7776a0ec457582050fa4320
unknown <Matthieu@.(none)>
2014-05-16 19:08:40 +0200

    [v0.2.2] Format php code

--- server/pronos/getpreviouspronos.php
+++ server/pronos/getpreviouspronos.php
@@ -20,22 +20,22 @@
 		$gameQuery = "SELECT * FROM games g INNER JOIN pronos p ON g.id_game = p.id_game WHERE g.schedule > '".$today."' AND p.userid = '".$userid."' ORDER BY g.schedule ASC";
 		foreach ($bdd -> query($gameQuery) as $row) {
 			$game = array(
-				addBy => $row['addBy'],
-				country => $row['country'],
-				id_game => $row['id_game'],
-				id_teamA => $row['id_teamA'],
-				id_teamB => $row['id_teamB'],
-				schedule => date('d/m/Y',$row['schedule']),
-				scheduleTime => date('G',$row['schedule']).'h'.date('i',$row['schedule']),
-				timestamp => $row['schedule'],
-				type => $row['type'],
-				prono => array(
-						id_prono => $row['id_prono'],
-						userid => $row['userid'],
-						scoreA => $row['scoreA'],
-						scoreB => $row['scoreB'],
-						prono_date => date('d/m/Y',$row['prono_date']),
-						prono_time => date('G',$row['prono_date']).'h'.date('i',$row['prono_date'])
+				'addBy' => $row['addBy'],
+				'country' => $row['country'],
+				'id_game' => $row['id_game'],
+				'id_teamA' => $row['id_teamA'],
+				'id_teamB' => $row['id_teamB'],
+				'schedule' => date('d/m/Y',$row['schedule']),
+				'scheduleTime' => date('G',$row['schedule']).'h'.date('i',$row['schedule']),
+				'timestamp' => $row['schedule'],
+				'type' => $row['type'],
+				'prono' => array(
+						'id_prono' => $row['id_prono'],
+						'userid' => $row['userid'],
+						'scoreA' => $row['scoreA'],
+						'scoreB' => $row['scoreB'],
+						'prono_date' => date('d/m/Y',$row['prono_date']),
+						'prono_time' => date('G',$row['prono_date']).'h'.date('i',$row['prono_date'])
 				)
 			);
 			
--- server/pronos/getpronos.php
+++ server/pronos/getpronos.php
@@ -16,39 +16,39 @@
 	$gameQuery = "SELECT * FROM games ORDER BY schedule DESC LIMIT 25";
 	foreach ($bdd -> query($gameQuery) as $row) {
 		$game = array(
-			addBy => $row['addBy'],
-			country => $row['country'],
-			id_game => $row['id_game'],
-			id_teamA => $row['id_teamA'],
-			id_teamB => $row['id_teamB'],
-			schedule => date('d/m/Y',$row['schedule']),
-			scheduleTime => date('G',$row['schedule']).'h'.date('i',$row['schedule']),
-			timestamp => $row['schedule'],
-			scoreA => $row['scoreA'],
-			scoreB => $row['scoreB'],
-			type => $row['type'],
-			pronos => array()
+			'addBy' => $row['addBy'],
+			'country' => $row['country'],
+			'id_game' => $row['id_game'],
+			'id_teamA' => $row['id_teamA'],
+			'id_teamB' => $row['id_teamB'],
+			'schedule' => date('d/m/Y',$row['schedule']),
+			'scheduleTime' => date('G',$row['schedule']).'h'.date('i',$row['schedule']),
+			'timestamp' => $row['schedule'],
+			'scoreA' => $row['scoreA'],
+			'scoreB' => $row['scoreB'],
+			'type' => $row['type'],
+			'pronos' => array()
 		);
 		
 		/* TODO Improve SQL request (too much calls) */
 		$pronoQuery ="SELECT * FROM pronos p INNER JOIN users u ON p.userid = u.userid WHERE p.id_game = '".$game['id_game']."' ORDER BY p.prono_date DESC";
 		foreach($bdd -> query($pronoQuery) as $data){
 			$user = array(
-				accreditation => $data["accreditation"],
-				email => $data["email"],
-				firstname => utf8_encode($data["firstname"]),
-				lastname => utf8_encode($data["lastname"]),
-				userid => $data["userid"]
+				'accreditation' => $data["accreditation"],
+				'email' => $data["email"],
+				'firstname' => utf8_encode($data["firstname"]),
+				'lastname' => utf8_encode($data["lastname"]),
+				'userid' => $data["userid"]
 			);
 			$prono = array(
-				id_game => $data["id_game"],
-				id_prono => $data["id_prono"],
-				prono_date => date('d/m/Y',$data['prono_date']),
-				prono_time => date('G',$data['prono_date']).'h'.date('i',$data['prono_date']),
-				timestamp => $data['prono_date'],
-				scoreA => $data["scoreA"],
-				scoreB => $data["scoreB"],
-				by => $user
+				'id_game' => $data["id_game"],
+				'id_prono' => $data["id_prono"],
+				'prono_date' => date('d/m/Y',$data['prono_date']),
+				'prono_time' => date('G',$data['prono_date']).'h'.date('i',$data['prono_date']),
+				'timestamp' => $data['prono_date'],
+				'scoreA' => $data["scoreA"],
+				'scoreB' => $data["scoreB"],
+				'by' => $user
 			);
 
 			array_push($game['pronos'],$prono);
