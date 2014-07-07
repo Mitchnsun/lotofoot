84b8768c6aea93447ce8defc0440881ee762734b
matthieu_comperat <matthieu.comperat@gmail.com>
2014-07-07 11:24:03 +0200

    [v0.2.4] Update top pronos when is ready

--- server/pronos/toppronosscores.php
+++ server/pronos/toppronosscores.php
@@ -65,57 +65,60 @@
 			);
 		}
 		
-		$data['pos_first'] = array_keys($response['bonus'][$id]['array'], $data['first']);
-		$data['pos_second'] = array_keys($response['bonus'][$id]['array'], $data['second']);
-		$data['pos_third'] = array_keys($response['bonus'][$id]['array'], $data['third']);
-		$data['pos_fourth'] = array_keys($response['bonus'][$id]['array'], $data['fourth']);
-		$data['pos_fifth'] = array_keys($response['bonus'][$id]['array'], $data['fifth']);
-		
-		// Set points for pronos
-		$points = 0;
-		
-		if($response['bonus'][$id]['top'] >= 1 && count($data['pos_first']) > 0){
-			$points += count($data['pos_first']);
-			if(array_search(0, $data['pos_first']) !== false){
-				$points += 4;
+		if($response['bonus'][$id]['ready'] == 1){
+			
+			$data['pos_first'] = array_keys($response['bonus'][$id]['array'], $data['first']);
+			$data['pos_second'] = array_keys($response['bonus'][$id]['array'], $data['second']);
+			$data['pos_third'] = array_keys($response['bonus'][$id]['array'], $data['third']);
+			$data['pos_fourth'] = array_keys($response['bonus'][$id]['array'], $data['fourth']);
+			$data['pos_fifth'] = array_keys($response['bonus'][$id]['array'], $data['fifth']);
+			
+			// Set points for pronos
+			$points = 0;
+			
+			if($response['bonus'][$id]['top'] >= 1 && count($data['pos_first']) > 0){
+				$points += count($data['pos_first']);
+				if(array_search(0, $data['pos_first']) !== false){
+					$points += 4;
+				}
 			}
-		}
-		if($response['bonus'][$id]['top'] >= 2 && count($data['pos_second']) > 0){
-			$points += count($data['pos_second']);
-			if(array_search(1, $data['pos_second']) !== false){
-				$points += 4;
+			if($response['bonus'][$id]['top'] >= 2 && count($data['pos_second']) > 0){
+				$points += count($data['pos_second']);
+				if(array_search(1, $data['pos_second']) !== false){
+					$points += 4;
+				}
 			}
-		}
-		if($response['bonus'][$id]['top'] >= 3 && count($data['pos_third']) > 0){
-			$points += count($data['pos_third']);
-			if(array_search(2, $data['pos_third']) !== false){
-				$points += 4;
+			if($response['bonus'][$id]['top'] >= 3 && count($data['pos_third']) > 0){
+				$points += count($data['pos_third']);
+				if(array_search(2, $data['pos_third']) !== false){
+					$points += 4;
+				}
 			}
-		}
-		if($response['bonus'][$id]['top'] >= 4 && count($data['pos_fourth']) > 0){
-			$points += count($data['pos_fourth']);
-			if(array_search(3, $data['pos_fourth']) !== false){
-				$points += 4;
+			if($response['bonus'][$id]['top'] >= 4 && count($data['pos_fourth']) > 0){
+				$points += count($data['pos_fourth']);
+				if(array_search(3, $data['pos_fourth']) !== false){
+					$points += 4;
+				}
 			}
-		}
-		if($response['bonus'][$id]['top'] >= 5 && count($data['pos_fifth']) > 0){
-			$points += count($data['pos_fifth']);
-			if(array_search(4, $data['pos_fifth']) !== false){
-				$points += 4;
+			if($response['bonus'][$id]['top'] >= 5 && count($data['pos_fifth']) > 0){
+				$points += count($data['pos_fifth']);
+				if(array_search(4, $data['pos_fifth']) !== false){
+					$points += 4;
+				}
 			}
+			
+			$data['points'] = $points;
+			
+			array_push($response['pronos'],$data);
+			
+			// Update pronos
+			$queryupdate = "UPDATE pronos_bonus SET points = :points, validation = '1' WHERE id = :id;";
+			$req = $bdd  -> prepare($queryupdate) or die(print_r($bdd->errorInfo()));
+			$req -> execute(array(
+					"points" => $points,
+					"id" => $row['id']
+			));
 		}
-		
-		$data['points'] = $points;
-		
-		array_push($response['pronos'],$data);
-		
-		// Update pronos
-		$queryupdate = "UPDATE pronos_bonus SET points = :points, validation = '1' WHERE id = :id;";
-		$req = $bdd  -> prepare($queryupdate) or die(print_r($bdd->errorInfo()));
-		$req -> execute(array(
-				"points" => $points,
-				"id" => $row['id']
-		));
 	}
 	
 	// return the JSON
