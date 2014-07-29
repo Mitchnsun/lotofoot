6f02e2e84614ffd7525e2cb674fe4608c48fdfcd
matthieu_comperat <matthieu.comperat@gmail.com>
2014-07-29 08:09:27 +0200

    [v0.3] Add stages for competitions

--- js/tmpl/pronos/nls/creategames.js
+++ js/tmpl/pronos/nls/creategames.js
@@ -33,8 +33,11 @@ define({
     /* Select List */
     hours : [20, 21, 22, 23, 14, 15, 16, 17, 18, 19, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
     minutes : ['00', '05', 15, 30, 45],
-    competitions : ["CM2014", "Friendly", "French-L1", "UEFA-CL", "UEFA-EL", "French-Cup"],
+    competitions : ["French-L1", "UEFA-CL", "UEFA-EL", "French-Cup", "Friendly"],
     "stage_CM2014" : ["Groups", "Last16", "Last8", "Semi", "ThirdPlace", "Final"],
+    "stage_French-L1" : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    "stage_UEFA-CL" : ["Round1", "Round2", "Round3", "Playoff", "Groups", "Last16", "Last8", "Semi", "Final"],
+    "stage_UEFA-EL" : ["Round1", "Round2", "Round3", "Playoff", "Groups", "Last32", "Last16", "Last8", "Semi", "Final"],
 
     /* Alert messages */
     add_success : "Merci, vos matchs ont bien été ajoutés et vous pouvez pronostiquer en retournant à l'accueil.",
