a28a8c2aabbc9aaad44822a909c072c3234933e6
matthieu_comperat <matthieu.comperat@gmail.com>
2014-08-31 19:10:31 +0200

    [v0.3] Initialize Admin page

---
+++ js/views/admin/adminview.js
@@ -0,0 +1,39 @@
+/**
+ * @author Matthieu Comperat
+ */
+// Filename: adminview.js
+define(['jquery', 'underscore', 'backbone', 'fmk/templateengine', 'fmk/lotofootapi',
+        'fmk/urls', 'i18n!tmpl/nls/admin', 'text!tmpl/adminpage.html'],
+function($, _, Backbone, te, LotofootApi, urls, i18n, tmpl) {
+  
+	var ClassView = Backbone.View.extend({
+		el : $('#container'),
+		initialize : function(options) {
+			this.user = options.user;
+			this.alertview = options.alertview;
+			this.teams = options.teams;
+			this.ranking = options.ranking;
+		},
+		render : function() {
+			var self = this;
+			$(this.el).html(te.renderTemplate(tmpl, {
+				i18n : i18n,
+				urls : urls
+			}));
+		},
+		/*
+		 * Events of the view
+		 */
+		events : {},
+		/*
+		 * Clean views and objects delegated to this view
+		 */
+		close : function() {
+			//this.activePronosView && (this.activePronosView.close ? this.activePronosView.close() : this.activePronosView.undelegateEvents());
+			this.undelegateEvents();
+		}
+	});
+
+	// Our module now returns our view
+	return ClassView;
+});
