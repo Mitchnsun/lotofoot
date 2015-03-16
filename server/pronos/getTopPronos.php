dab521cef2875f1550e37f686a84dc9b63087e74
matthieu_comperat <matthieu.comperat@gmail.com>
2015-03-16 20:34:16 +0100

    [v0.3] return all bonus

--- server/pronos/getTopPronos.php
+++ server/pronos/getTopPronos.php
@@ -13,52 +13,58 @@
   
   $response['status'] = 200;
   $today = time();
-  $id_bonus = $_GET['id_bonus'];
+	$id_bonus = 2;
 	
 	// Get Bonus
-	$querybonus = "SELECT * FROM bonus WHERE id_bonus=:id_bonus";
+	$querybonus = "SELECT * FROM bonus ORDER BY dead_line DESC";
 	$reqbonus = $bdd -> prepare($querybonus) or die(json_encode(array("status" => 500, "errorCode" => "BDLogin", "message" => $bdd->errorInfo())));
 	$reqbonus -> execute(array("id_bonus" => $id_bonus));
-	$result = $reqbonus -> fetch();
-	$response['bonus'] = array(
-		"id_bonus" => $result['id_bonus'],
-		"name" => $result['name'],
-		"id_description" => $result['id_description'],
-		"table_link" => $result['table_link'],
-		"dead_line" => $result['dead_line'],
-		"ready" => $result['ready'],
-		"top" => $result['top'],
-		"first" => $result['first'],
-		"second" => $result['second'],
-		"third" => $result['third'],
-		"fourth" => $result['fourth'],
-		"fifth" => $result['fifth'],
-		"type" => $result['type'],
-		"season" => $result['season']
-	);
 	
-  $response['pronos'] = array();
-
-  $query = "SELECT * FROM pronos_bonus p INNER JOIN users u ON p.userid = u.userid WHERE p.id_bonus = :id_bonus ORDER BY p.at DESC";
-	$req = $bdd -> prepare($query) or die(json_encode(array("status" => 500, "errorCode" => "BDLogin", "message" => $bdd->errorInfo())));
-  $req -> execute(array("id_bonus" => $id_bonus));
-  while($data = $req -> fetch()){
-    $prono = array(
-      "schedule" => date('d/m/Y',$data['at']),
-			"scheduleTime" => date('G',$data['at']).'h'.date('i',$data['at']),
-      "first" => $data['first'],
-      "second" => $data['second'],
-      "third" => $data['third'],
-      "fourth" => $data['fourth'],
-      "fifth" => $data['fifth'],
-      "userid" => $data['userid'],
-      "validation" => $data['validation'],
-      "points" => $data['points'],
-      "displayName" => utf8_encode($data['firstname']).' '.utf8_encode($data['lastname'])
-    );
+	$response['bonus'] = array();
+	while($result = $reqbonus -> fetch()){
+		$bonus = array(
+			"id_bonus" => $result['id_bonus'],
+			"name" => $result['name'],
+			"id_description" => $result['id_description'],
+			"table_link" => $result['table_link'],
+			"dead_line" => $result['dead_line'],
+			"ready" => $result['ready'],
+			"top" => $result['top'],
+			"first" => $result['first'],
+			"second" => $result['second'],
+			"third" => $result['third'],
+			"fourth" => $result['fourth'],
+			"fifth" => $result['fifth'],
+			"type" => $result['type'],
+			"season" => $result['season']
+		);
 		
-		array_push($response['pronos'], $prono);
-  }
+		$id_bonus = $result['id_bonus'];
+		$bonus['pronos'] = array();
+
+	  $query = "SELECT * FROM pronos_bonus p INNER JOIN users u ON p.userid = u.userid WHERE p.id_bonus = :id_bonus ORDER BY p.at DESC";
+		$req = $bdd -> prepare($query) or die(json_encode(array("status" => 500, "errorCode" => "BDLogin", "message" => $bdd->errorInfo())));
+	  $req -> execute(array("id_bonus" => $id_bonus));
+	  while($data = $req -> fetch()){
+	    $prono = array(
+	      "schedule" => date('d/m/Y',$data['at']),
+				"scheduleTime" => date('G',$data['at']).'h'.date('i',$data['at']),
+	      "first" => $data['first'],
+	      "second" => $data['second'],
+	      "third" => $data['third'],
+	      "fourth" => $data['fourth'],
+	      "fifth" => $data['fifth'],
+	      "userid" => $data['userid'],
+	      "validation" => $data['validation'],
+	      "points" => $data['points'],
+	      "displayName" => utf8_encode($data['firstname']).' '.utf8_encode($data['lastname'])
+	    );
+			
+			array_push($bonus['pronos'], $prono);
+	  }
+	  
+		array_push($response['bonus'], $bonus);
+	}
 	
 	$response['time'] = $today;
 	
